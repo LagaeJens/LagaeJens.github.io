79d382e063b447175de2c241daedb57eae558902
Jens Lagae <jens.lagae@student.howest.be>
2022-12-21 18:42:55 +0100

    commi

--- script/app.js
+++ script/app.js
@@ -74,28 +74,194 @@ let showUpcoming = function (jsonObject) {
     let lpad = getLaunchpad(pad); //pad is de parameter
     console.log(jsonObject.crew[0].crew);
     //crw ophalen
-    // let crew = getCrew(jsonObject.crew[0].crew);
-    // let crew1 = getCrew(jsonObject.crew[1].crew);
-    let crew2 = getCrew(jsonObject.crew[2].crew);
-    // let crew3 = getCrew(jsonObject.crew[3].crew);
-
-
-
+    let crew = getCrew(jsonObject.crew[0].crew);
+    let crew1 = getCrew1(jsonObject.crew[1].crew);
+    let crew2 = getCrew2(jsonObject.crew[2].crew);
+    let crew3 = getCrew3(jsonObject.crew[3].crew);
 }
-//5e9e4502f509094188566f88
+//store all crew members in an array
+// let crewMembers = [crew, crew1, crew2, crew3];
 
 
-//crew ophalen en tonen
-let showCrew = function (crew) {
+
+const showCrewInfo = function (crew) {
     console.log(crew);
-    document.querySelector('.crewName').innerHTML = crew.name;
+    let htmlstring = "";
+    htmlstring = `<article class="o-section o-section--xl">
+                <div class="o-layout o-layout--gutter-xl o-layout--justify-center o-layout--align-center">
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <figure class="u-mb-lg">
+                            <picture>
+                                <img class="crewImage img" src="" alt="Picture" />
+                            </picture>
+                        </figure>
+                    </div>
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <div class="u-max-width-sm">
+
+                            <h2 class="crewName">${crew.name}</h2>
+                            <ol class="c-lead c-lead--lg u-color-neutral-dark u-mb-lg">
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+
+                            </ol>
+                        </div>
+                    </div>
+            </article>`
+    document.querySelector('.crewInfo').innerHTML = htmlstring;
 }
+
+const showCrewInfo1 = function (crew) {
+    console.log(crew);
+    let htmlstring = "";
+    htmlstring += `<article class="o-section o-section--xl">
+                <div class="o-layout o-layout--gutter-xl o-layout--justify-center o-layout--align-center">
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <figure class="u-mb-lg">
+                            <picture>
+                                <img class="crewImage img" src="" alt="Picture" />
+                            </picture>
+                        </figure>
+                    </div>
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <div class="u-max-width-sm">
+
+                            <h2 class="crewName">${crew.name}</h2>
+                            <ol class="c-lead c-lead--lg u-color-neutral-dark u-mb-lg">
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+
+                            </ol>
+                        </div>
+                    </div>
+            </article>`
+    document.querySelector('.crewInfo').innerHTML = htmlstring;
+}
+
+
+const showCrewInfo2 = function (crew) {
+    console.log(crew);
+    let htmlstring = "";
+    htmlstring = `
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <div class="u-max-width-sm">
+
+                            <h2 class="crewName">${crew.name}</h2>
+                            <ol class="c-lead c-lead--lg u-color-neutral-dark u-mb-lg">
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+
+                            </ol>
+                        </div>
+                    </div>
+            </article>`
+    document.querySelector('.crewInfo').innerHTML = htmlstring;
+}
+
+const showCrewInfo3 = function (crew) {
+    console.log(crew);
+    let htmlstring = "";
+    htmlstring = `<article class="o-section o-section--xl">
+                <div class="o-layout o-layout--gutter-xl o-layout--justify-center o-layout--align-center">
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <figure class="u-mb-lg">
+                            <picture>
+                                <img class="crewImage img" src="${crew.}" alt="Picture" />
+                            </picture>
+                        </figure>
+                    </div>
+                    <div class="o-layout__item u-2-of-3-bp2 u-1-of-2-bp3">
+                        <div class="u-max-width-sm">
+
+                            <h2 class="crewName">${crew.name}</h2>
+                            <ol class="c-lead c-lead--lg u-color-neutral-dark u-mb-lg">
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+                                <ul>test</ul>
+                                <ul>test2</ul>
+
+                            </ol>
+                        </div>
+                    </div>
+            </article>`
+    document.querySelector('.crewInfo').innerHTML = htmlstring;
+}
+
+
+
 
 //get crew data from spaxeX API
 let getCrew = (crew) => {
-    handleData(`https://api.spacexdata.com/v4/crew/${crew}`, showCrew, null, 'GET')
+    handleData(`https://api.spacexdata.com/v4/crew/${crew}`, listenToButton, null, 'GET')
+
+}
+//get crew data from spaxeX API
+let getCrew1 = (crew) => {
+    handleData(`https://api.spacexdata.com/v4/crew/${crew}`, listenToButton1, null, 'GET')
+
+}
+//get crew data from spaxeX API
+let getCrew2 = (crew) => {
+    handleData(`https://api.spacexdata.com/v4/crew/${crew}`, listenToButton2, null, 'GET')
+
+}
+//get crew data from spaxeX API
+let getCrew3 = (crew) => {
+    handleData(`https://api.spacexdata.com/v4/crew/${crew}`, listenToButton3, null, 'GET')
+
+}
 
 
+// register button
+const listenToButton = function (crew) {
+    console.log('IK WERK!')
+    let button = document.querySelector('.js-button')
+    console.log(button)
+    button.addEventListener('click', function () {
+        console.log('IK WERK OOK!')
+        showCrewInfo(crew);
+    })
+}
+const listenToButton1 = function (crew) {
+    console.log('IK WERK!')
+    let button = document.querySelector('.js-button1')
+    console.log(button)
+    button.addEventListener('click', function () {
+        console.log('IK WERK OOK!')
+        showCrewInfo1(crew);
+    })
+}
+const listenToButton2 = function (crew) {
+    console.log('IK WERK!')
+    let button = document.querySelector('.js-button2')
+    console.log(button)
+    button.addEventListener('click', function () {
+        console.log('IK WERK OOK!')
+        showCrewInfo2(crew);
+    })
+}
+const listenToButton3 = function (crew) {
+    console.log('IK WERK!')
+    let button = document.querySelector('.js-button3')
+    console.log(button)
+    button.addEventListener('click', function () {
+        console.log('IK WERK OOK!')
+        showCrewInfo3(crew);
+    })
 }
 
 
@@ -114,6 +280,11 @@ let getLaunchpad = (pad) => {
 }
 document.addEventListener('DOMContentLoaded', function () {
     getMission();
+    // listenToButton();
+    // listenToButton1();
+    // listenToButton2();
+    // listenToButton3();
+    // showCrewInfo();
     // getLaunchpad();
     // getCrew("5ebf1b7323a9a60006e03a7b");
 });
