9759c727a3443eb66c5cb076060fa80f5f750a78
Jens Lagae <jens.lagae@student.howest.be>
2022-12-22 11:12:33 +0100

    _

--- script/app.js
+++ script/app.js
@@ -231,9 +231,9 @@ let convertData = (data) => {
         label: 'Number of launches',
         data: Object.values(counts),
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)'
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          'rgba(255, 206, 86, 0.8)'
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
